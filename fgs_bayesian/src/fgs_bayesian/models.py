c08970a252cc65253bf997821b9b96b8b5d51f9a
fugashy <fugashy@icloud.com>
2019-04-03 20:04:47 +0900

    Modify member of Shopping model to update repeatedly

--- fgs_bayesian/src/fgs_bayesian/models.py
+++ fgs_bayesian/src/fgs_bayesian/models.py
@@ -6,23 +6,24 @@ class Shopping(object):
     def __init__(self):
         # 事前確率
         # 買う確率, 買わない確率
-        self.prior = np.array([0.2, 0.8])
+        self.probability = np.array([0.2, 0.8])
 
+    def get_probability(self):
+        return self.probability
+
+    def update(self):
         # 条件付き確率
         # 買う人が声をかける確率・買わない人が声をかける確率
         # 買う人が声をかけない確率・買わない人が声をかけない確率
-        self.conditional = np.array([[0.9, 0.3], [0.1, 0.7]])
+        conditional = np.array([[0.9, 0.3], [0.1, 0.7]])
 
-    def update(self):
         # 声をかけた世界
         # 声をかけない世界を消す
-        self.conditional[1] = 0.
+        conditional = np.delete(conditional, 1, 0)
 
         # 更新
-        posterior_raw = self.prior * self.conditional
+        posterior_raw = self.probability * conditional
 
         # 比例関係を保ったまま正規化条件を回復
         weight = np.max(posterior_raw[:, ::-1] + posterior_raw)
-        posterior = posterior_raw / weight
-
-        return posterior
+        self.probability = posterior_raw / weight
