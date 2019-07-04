c8cba4c9dbae6e740593a7384ba61693a9d51e12
fugashy <fugashy@icloud.com>
2019-07-04 21:38:40 +0900

    Fix plotter variables

--- fgs_gaussian_filter/fgs_gaussian_filter/plotter.py
+++ fgs_gaussian_filter/fgs_gaussian_filter/plotter.py
@@ -7,32 +7,32 @@ import numpy as np
 
 class EKFHistory(object):
     def __init__(self):
-        self.hxTrue = np.zeros((4, 1))
-        self.hxEst = np.zeros((4, 1))
-        self.hxDR = np.zeros((4, 1))
-        self.hz = np.zeros((2, 1))
+        self._x_gt = np.zeros((4, 1))
+        self._x_est = np.zeros((4, 1))
+        self._x_noised = np.zeros((4, 1))
+        self._z = np.zeros((2, 1))
 
-    def plot(self, xTrue, xEst, xDR, z, PEst):
-        self.hxTrue = np.hstack((self.hxTrue, xTrue))
-        self.hxEst = np.hstack((self.hxEst, xEst))
-        self.hxDR = np.hstack((self.hxDR, xDR))
-        self.hz = np.hstack((self.hz, z))
+    def plot(self, x_gt, x_est, x_noised, z, x_cov_est):
+        self._x_gt = np.hstack((self._x_gt, x_gt))
+        self._x_est = np.hstack((self._x_est, x_est))
+        self._x_noised = np.hstack((self._x_noised, x_noised))
+        self._z = np.hstack((self._z, z))
 
         plt.cla()
-        plt.plot(self.hz[0, :], self.hz[1, :], ".g")
-        plt.plot(self.hxTrue[0, :].flatten(),
-                 self.hxTrue[1, :].flatten(), "-b")
-        plt.plot(self.hxDR[0, :].flatten(),
-                 self.hxDR[1, :].flatten(), "-k")
-        plt.plot(self.hxEst[0, :].flatten(),
-                 self.hxEst[1, :].flatten(), "-r")
-        self.plot_covariance_ellipse(xEst, PEst)
-        plt.axis("equal")
+        plt.plot(self._z[0, :], self._z[1, :], '.g')
+        plt.plot(self._x_gt[0, :].flatten(),
+                 self._x_gt[1, :].flatten(), '-b')
+        plt.plot(self._x_noised[0, :].flatten(),
+                 self._x_noised[1, :].flatten(), '-k')
+        plt.plot(self._x_est[0, :].flatten(),
+                 self._x_est[1, :].flatten(), '-r')
+        self.plot_covariance_ellipse(x_est, x_cov_est)
+        plt.axis('equal')
         plt.grid(True)
         plt.pause(0.001)
 
-    def plot_covariance_ellipse(self, xEst, PEst):  # pragma: no cover
-        Pxy = PEst[0:2, 0:2]
+    def plot_covariance_ellipse(self, x_est, x_cov_est):  # pragma: no cover
+        Pxy = x_cov_est[0:2, 0:2]
         eigval, eigvec = np.linalg.eig(Pxy)
 
         if eigval[0] >= eigval[1]:
@@ -51,6 +51,6 @@ class EKFHistory(object):
         R = np.array([[math.cos(angle), math.sin(angle)],
                       [-math.sin(angle), math.cos(angle)]])
         fx = R@(np.array([x, y]))
-        px = np.array(fx[0, :] + xEst[0, 0]).flatten()
-        py = np.array(fx[1, :] + xEst[1, 0]).flatten()
-        plt.plot(px, py, "--r")
+        px = np.array(fx[0, :] + x_est[0, 0]).flatten()
+        py = np.array(fx[1, :] + x_est[1, 0]).flatten()
+        plt.plot(px, py, '--r')
