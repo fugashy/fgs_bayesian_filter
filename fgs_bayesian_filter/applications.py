0fcc20e73ea07d21c4911e8f5c98270a8f8dab9f
fugashy <fugashy@icloud.com>
2019-07-09 21:56:37 +0900

    Add publisher that is in charge of dead-reckoning data

--- fgs_bayesian_filter/applications.py
+++ fgs_bayesian_filter/applications.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import time
 import numpy as np
 
 import rclpy
@@ -37,15 +38,19 @@ class EstimatedStatePublisherNode(rclpy.node.Node):
 
         # Publishers
         # Current states
+        # NOTE(fugashy) At 20190709, PoseWithCovariance cannot be visualized on RViz2
         self._pub_x_est = self.create_publisher(PoseWithCovarianceStamped, 'estimated_state', 1)
         self._pub_x_gt = self.create_publisher(PoseWithCovarianceStamped, 'groundtruth_state', 1)
         # History
         self._pub_x_est_array = self.create_publisher(PoseArray, 'estimated_state_array', 1)
         self._pub_x_gt_array = self.create_publisher(PoseArray, 'groundtruth_state_array', 1)
+        self._pub_x_dr_array = self.create_publisher(PoseArray, 'dead_reckoning_state_array', 1)
         self._x_est_array_msg = PoseArray()
         self._x_est_array_msg.header.frame_id = 'map'
         self._x_gt_array_msg = PoseArray()
         self._x_gt_array_msg.header.frame_id = 'map'
+        self._x_dr_array_msg = PoseArray()
+        self._x_dr_array_msg.header.frame_id = 'map'
 
     def run(self):
         try:
@@ -67,10 +72,15 @@ class EstimatedStatePublisherNode(rclpy.node.Node):
                 self._x_est_array_msg.poses.append(x_est_msg.pose.pose)
                 x_gt_msg = ros_utils.ConvertEstimated2DPoseAsROSMessage(self._x_gt)
                 self._x_gt_array_msg.poses.append(x_gt_msg.pose.pose)
+                x_dr_msg = ros_utils.ConvertEstimated2DPoseAsROSMessage(self._x_noised)
+                self._x_dr_array_msg.poses.append(x_dr_msg.pose.pose)
 
                 self._pub_x_est.publish(x_est_msg)
                 self._pub_x_est_array.publish(self._x_est_array_msg)
                 self._pub_x_gt.publish(x_gt_msg)
                 self._pub_x_gt_array.publish(self._x_gt_array_msg)
+                self._pub_x_dr_array.publish(self._x_dr_array_msg)
+
+                time.sleep(0.3)
         except KeyboardInterrupt:
             print('Interrupted by user')
