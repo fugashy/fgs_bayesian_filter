1a2fa038df861a106af8f1912ca4b3abed6298ac
fugashy <fugashy@icloud.com>
2019-07-15 22:30:59 +0900

    Modify offline simulator to apply failure of observation

--- fgs_bayesian_filter/offline_simulator.py
+++ fgs_bayesian_filter/offline_simulator.py
@@ -18,7 +18,8 @@ class OfflineSimulator():
                 command_models,
                 motion_models,
                 observation_models,
-                plotters
+                plotters,
+                sampling_modulator
             )
         else:
             import bayesian_filters
@@ -26,10 +27,14 @@ class OfflineSimulator():
             import motion_models
             import observation_models
             import plotters
+            import sampling_modulator
 
         self._motion_model = motion_models.create(config['motion_model'])
         self._obs_model = observation_models.create(config['observation_model'])
         self._command_model = command_models.create(config['command_model'])
+        self._sampler = None
+        if 'sampler' in config:
+            self._sampler = sampling_modulator.create(config['sampler'])
         self._filter = bayesian_filters.create(
             config['bayesian_filter'], self._command_model, self._motion_model, self._obs_model)
         self._plotter = plotters.create(config['plotter'], self._obs_model)
@@ -46,7 +51,9 @@ class OfflineSimulator():
 
                 # 操作と観測
                 u_noised = self._command_model.command(with_noise=True)
-                z_noised = self._obs_model.observe_at(self._x_gt, with_noise=True)
+                z_noised = None
+                if self._sampler is None or self._sampler.is_valid():
+                    z_noised = self._obs_model.observe_at(self._x_gt, with_noise=True)
 
                 # ノイズを受けた状態・操作を用いて運動モデルを更新する(こいつはこれの繰り返し)
                 self._x_noised = self._motion_model.calc_next_motion(
