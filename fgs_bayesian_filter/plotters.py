18afc5b32c5b57597265a9a031b01b599242f6b5
fugashy <fugashy@icloud.com>
2019-07-15 22:32:27 +0900

    Change color of plotter

--- fgs_bayesian_filter/plotters.py
+++ fgs_bayesian_filter/plotters.py
@@ -48,7 +48,7 @@ class Plotter():
         fx = R@(np.array([x, y]))
         px = np.array(fx[0, :] + x_est[0, 0]).flatten()
         py = np.array(fx[1, :] + x_est[1, 0]).flatten()
-        plt.plot(px, py, '--r')
+        plt.plot(px, py, '--g')
 
 
 class SimplePlotter(Plotter):
@@ -63,16 +63,18 @@ class SimplePlotter(Plotter):
         self._x_gt_hist = np.hstack((self._x_gt_hist, x_gt))
         self._x_est_hist = np.hstack((self._x_est_hist, x_est))
         self._x_noised_hist = np.hstack((self._x_noised_hist, x_noised))
-        self._z_hist = np.hstack((self._z_hist, z))
+        # 観測ができなかった場合を考慮
+        if z is not None:
+            self._z_hist = np.hstack((self._z_hist, z))
 
         plt.cla()
         plt.plot(self._z_hist[0, :], self._z_hist[1, :], '.g')
         plt.plot(self._x_gt_hist[0, :].flatten(),
                  self._x_gt_hist[1, :].flatten(), '-b')
         plt.plot(self._x_noised_hist[0, :].flatten(),
-                 self._x_noised_hist[1, :].flatten(), '-k')
+                 self._x_noised_hist[1, :].flatten(), '-r')
         plt.plot(self._x_est_hist[0, :].flatten(),
-                 self._x_est_hist[1, :].flatten(), '-r')
+                 self._x_est_hist[1, :].flatten(), '-g')
         self._plot_covariance_ellipse(x_est, x_cov_est)
         plt.axis('equal')
         plt.grid(True)
@@ -87,24 +89,26 @@ class PlotterWithLandMark(Plotter):
         self._x_noised = np.zeros((4, 1))
         self._landmarks = obs_model.landmark
 
-    def plot(self, x_gt, x_est, x_noised, z, x_cov_est):
+    def plot(self, x_gt, x_est, x_noised, z, x_cov_est, px):
         self._x_gt = np.hstack((self._x_gt, x_gt))
         self._x_est = np.hstack((self._x_est, x_est))
         self._x_noised = np.hstack((self._x_noised, x_noised))
 
         plt.cla()
 
-        for i in range(len(z[:, 0])):
-            plt.plot([x_gt[0, 0], z[i, 1]], [x_gt[1, 0], z[i, 2]], '-k')
+        if z is not None:
+            for i in range(len(z[:, 0])):
+                plt.plot([x_gt[0, 0], z[i, 1]], [x_gt[1, 0], z[i, 2]], '-g')
 
-        plt.plot(self._landmarks[:, 0], self._landmarks[:, 1], '*k')
+        plt.plot(self._landmarks[:, 0], self._landmarks[:, 1], '*g')
 
         plt.plot(self._x_gt[0, :].flatten(),
                  self._x_gt[1, :].flatten(), '-b')
         plt.plot(self._x_noised[0, :].flatten(),
-                 self._x_noised[1, :].flatten(), '-k')
+                 self._x_noised[1, :].flatten(), '-r')
         plt.plot(self._x_est[0, :].flatten(),
-                 self._x_est[1, :].flatten(), '-r')
+                 self._x_est[1, :].flatten(), '-g')
+        plt.plot(px[0, :], px[1, :], ".g")
         self._plot_covariance_ellipse(x_est, x_cov_est)
         plt.axis('equal')
         plt.grid(True)
